f33b9f0f3302c8c82e9e1a23cc1b0c95902f19b5
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-30 12:54:39 -0500

    MVP - GET /api/notes/:id - 1) Added the route in noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -11,4 +11,20 @@ router.get('/', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+router.get('/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const note = await notes.getById(id);
+
+        if(note) {
+            res.status(200).json(note)
+        } else {
+            res.status(404).json({MESSAGE: `NOTE NOT FOUND`})
+        }
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
