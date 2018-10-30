5c7879da80c4b1f3f872d1e976ac7ab525695c2c
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-30 16:13:20 -0500

    MVP - DELETE /api/notes/:id - 1) Made some minor changes with the try-catches in routes/noteRoutes.js 2) Added DELETE endpoint in routes/noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -22,9 +22,8 @@ router.get('/:id', async (req, res) => {
             res.status(404).json({MESSAGE: `NOTE NOT FOUND`})
         }
     }
-    catch(err) {
-        res.status(500).json(err);
-    }
+
+    catch(err) {res.status(500).json(err)}
 });
 
 router.post('/', async (req, res) => {
@@ -32,9 +31,9 @@ router.post('/', async (req, res) => {
 
     try {
         if (!title.substr(1, 200)) {
-            res.status(400).json({REQUIRED: `PLEASE INCLUDE A TITLE LESS THAN 200 CHARACTERS`});
+            res.status(411).json({REQUIRED: `PLEASE INCLUDE A TITLE LESS THAN 200 CHARACTERS`});
         } else if (!content) {
-            res.status(400).json({REQUIRED: `PLEASE INCLUDE CONTENT`})
+            res.status(411).json({REQUIRED: `PLEASE INCLUDE CONTENT`})
         }else {
             notes 
                 .insert({title, content})
@@ -43,9 +42,8 @@ router.post('/', async (req, res) => {
                 })
         }
     }
-    catch(err){
-        res.status(500).json(err);
-    }
+
+    catch(err){res.status(500).json(err)}
 });
 
 router.put('/:id', async (req, res) => {
@@ -53,19 +51,42 @@ router.put('/:id', async (req, res) => {
     const {title, content} = await req.body;
 
     try {
+        if (!title.substr(1, 200)) {
+            res.status(411).json({REQUIRED: `PLEASE INCLUDE A TITLE LESS THAN 200 CHARACTERS`});
+        } else if (!content) {
+            res.status(411).json({REQUIRED: `PLEASE INCLUDE CONTENT`})
+        }else {
+            notes
+                .update(id, {title, content})
+                .then(isUpdated => {
+                    if (!isUpdated){
+                        res.status(400).json({ERROR: `NOTE WAS NOT ABLE TO UPDATE`});
+                    } else {
+                        res.status(200).json(isUpdated);
+                    }
+                })
+        }
+    }
+
+    catch(err) {res.status(500).json(err)}
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = await req.params;
+
+    try {
         notes
-            .update(id, {title, content})
-            .then(isUpdated => {
-                if (!isUpdated){
-                    res.status(400).json({ERROR: `NOTE WAS NOT ABLE TO UPDATE`});
-                } else {
-                    res.status(200).json(isUpdated);
-                }
-            })
+        .remove(id)
+        .then(isRemoved => {
+            if (!isRemoved) {
+                res.status(400).json({ERROR: `NOTE WAS NOT ABLE TO BE REMOVED`});
+            } else {
+                res.status(200).json(isRemoved);
+            }
+        })
     }
-    catch(err) {
-        res.status(500).json(err);
-    }
+
+    catch(err){res.status(500).json(err)}
 });
 
 module.exports = router;
